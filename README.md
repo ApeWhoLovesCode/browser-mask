715c3c22dc7fa4ac2979de48cae73070ebbb040a
lhh <970519495@qq.com>
2024-08-01 10:17:04 +0800

    fix: 完善 mask 的 css 以及部分文档

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 专用于调整浏览器中某个页面的亮度。
 
+| 描述  | 快捷键         |
+| ----- | -------------- |
+| 开/关 | Ctrl Shift M   |
+| 亮度  | Ctrl Shift +/- |
+
 ## 使用
 
-将该 [压缩包](https://github.com/ApeWhoLovesCode/browser-mask/tree/main/build/chrome-mv3-prod.zip) 下载，解压后将文件夹拖拽添加到浏览器的扩展程序中即可使用。
+[下载 browser-mask](https://github.com/ApeWhoLovesCode/browser-mask/tree/main/build/browser-mask.zip) 。
 
+将下载后的压缩包解压后，将文件夹拖拽添加到浏览器的扩展程序中即可使用。
--- contents/mask.tsx
+++ contents/mask.tsx
@@ -1,82 +1,74 @@
-import cssText from "data-text:~/contents/style.css"
-import './style.css';
-import { useStorage } from "@plasmohq/storage/hook"
-import type { MaskState } from '~type';
+import "./style.css";
+import { useStorage } from "@plasmohq/storage/hook";
+import type { MaskState } from "~type";
 import { MASK_STORAGE } from "~common/storageKey";
 import { useEffect, useState } from "react";
-import React from "react";
 import { isIncludesId } from "~utils/tab";
 import { rangeOpacity } from "~utils/range";
-
-export const getStyle = () => {
-  const style = document.createElement("style")
-  style.textContent = cssText
-  return style
-}
 
 export default function Mask() {
   const [state, setState] = useStorage<Partial<MaskState>>(MASK_STORAGE, {
     isOpen: false,
     tabIds: [],
     opacity: 40,
-  })
+  });
 
-  const [url, setUrl] = useState('')
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
-    const hostUrl = window.location.host
-    setUrl(hostUrl)
+    const hostUrl = window.location.host;
+    setUrl(hostUrl);
 
     const changeOpacity = (v: number) => {
-      setState(data => {
-        if(!data.isOpen) return data
-        data.opacity = rangeOpacity(data.opacity + v)
-        return { ...data }
-      })
-    }
+      setState((data) => {
+        if (!data.isOpen) return data;
+        data.opacity = rangeOpacity(data.opacity + v);
+        return { ...data };
+      });
+    };
 
     const onKeyDown = (e: KeyboardEvent) => {
       // e.metaKey: true 代表 Mac command
-      const isCtrlAndShift = e.ctrlKey && e.shiftKey
-      if(isCtrlAndShift && (e.key === 'm' || e.key === 'M')) {
-        setState(data => {
-          if(!isIncludesId(hostUrl, data.tabIds)) {
-            if(!data.tabIds) data.tabIds = []
-            data.tabIds.push(hostUrl)
-            data.isOpen = true
+      const isCtrlAndShift = e.ctrlKey && e.shiftKey;
+      if (isCtrlAndShift && (e.key === "m" || e.key === "M")) {
+        setState((data) => {
+          if (!isIncludesId(hostUrl, data.tabIds)) {
+            if (!data.tabIds) data.tabIds = [];
+            data.tabIds.push(hostUrl);
+            data.isOpen = true;
           } else {
-            data.isOpen = !data.isOpen
+            data.isOpen = !data.isOpen;
+            data.tabIds = [];
           }
-          return { ...data }
-        })
-      } else if(isCtrlAndShift) {
-        if(e.key === '=' || e.key === '+') {
-          changeOpacity(10)
-        } else if(e.key === '-' || e.key === '_') {
-          changeOpacity(-10)
+          return { ...data };
+        });
+      } else if (isCtrlAndShift) {
+        if (e.key === "=" || e.key === "+") {
+          changeOpacity(10);
+        } else if (e.key === "-" || e.key === "_") {
+          changeOpacity(-10);
         }
       }
-    }
+    };
 
-    window.addEventListener('keydown', onKeyDown);
+    window.addEventListener("keydown", onKeyDown);
     return () => {
-      window.removeEventListener('keydown', onKeyDown);
-    }
-  }, [])
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, []);
 
-  const isOpen = state.isOpen && isIncludesId(url, state.tabIds)
+  const isOpen = state.isOpen && isIncludesId(url, state.tabIds);
 
   return (
     <>
       {isOpen && (
-        <div 
-          className="fixed -top-1 -left-1 size-0 transition-all z-[999999]" 
-          style={{ 
-            boxShadow: `55vmax 55vmax 0 55vmax rgba(0, 0, 0, ${state.opacity / 100})`
+        <div
+          className="browser-mask"
+          style={{
+            boxShadow: `55vmax 55vmax 0 55vmax rgba(0, 0, 0, ${state.opacity / 100})`,
           }}
-        >
-        </div>
+        ></div>
       )}
     </>
-  )
+  );
 }
